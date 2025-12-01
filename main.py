6c68c5e4a261bcb714fed8e1368e720b59e0a0c5
Harvin Ong <harvin.ong20@gmail.com>
2025-12-01 17:22:12 +0800

    Implemented Flask.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import webbrowser
+from random import random
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def process():
+    return "Test"
+
+@app.route('/rand/')
+def random_num():
+    return f"{random():.2%}"
 
 def main():
     webbrowser.open("index.html")
+    app.run(host = "localhost", port = 8000, debug = True)
+
 if __name__ == '__main__':
     main()
