1cc4a679b7500411d1fa372238f921fc46954e22
Harvin Ong <harvin.ong20@gmail.com>
2025-12-02 15:49:55 +0800

    Used Flask's HTML inheritance architecture.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import webbrowser
 from random import random, randint
-from flask import Flask
+from flask import Flask, render_template, url_for
 from flask_cors import cross_origin
 from requests import get
 from json import loads
@@ -13,6 +13,11 @@ def process_user_input(prompt):
     r = get("https://randomfox.ca/floof")
     d: dict[str, str] = loads(r.content)
     return prompt[::-1] + f"<br><img src='{d['image']}' height=128>"
+
+@app.route('/')
+def index():
+    # print("fetching index.html")
+    return render_template('index.html')
 
 def main():
     # webbrowser.open("index.html")
