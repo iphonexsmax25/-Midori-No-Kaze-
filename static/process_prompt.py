bbbaa50e92e74f2aafd4b0dbf9a319efa028c919
Harvin Ong <harvin.ong20@gmail.com>
2025-12-02 22:06:49 +0800

    Random response presets to test message box.

--- static/process_prompt.py
+++ static/process_prompt.py
@@ -3,27 +3,41 @@ from random import random, randint
 from requests import get
 from json import loads
 
+# HTML Attributes
 def compile_attributes(**attributes: Any) -> str:
     att_pairs: list[str] = []
     for key, value in attributes.items():
         att_pairs.append(f"{key}={value}")
     return " ".join(att_pairs)
 
+# HTML Element
 def hyperlink(url: str, text: str|None = None, **attributes: Any) -> str:
     return f"<a href=\"{url}\" {compile_attributes(**attributes)}>{text if text else url}</a>"
 
 def image(source: str, **attributes: Any) -> str:
     return f"<img src='{source}' {compile_attributes(**attributes)}>"
 
-def process(prompt: str) -> str:
+def encase(element: str, text: str) -> str:
+    return f"<{element}>{text}</{element}>"
+
+# Presets
+def preset_1(prompt: str) -> str:
     r = get("https://randomfox.ca/floof")
     d: dict[str, str] = loads(r.content)
-    return \
-        prompt[::-1] + f" {random():.1%}<br>\
-        {image(d['image'], height = 128)}<br>\
-        {hyperlink(d['image'], style = "color:red")}"
+    return f"{prompt[::-1]} {encase("b", f"{random():.1%}")}<br>{image(d['image'], height = 128)}<br>{hyperlink(d['image'], style = "color:red")}"
+
+def preset_2(prompt: str) -> str:
+    cat_gif = image("https://cataas.com/cat/gif", height = 128)
+    cat_fact_resp = get("https://meowfacts.herokuapp.com/")
+    cat_fact: str = loads(cat_fact_resp.content)['data'][0]
+    return encase("h1", "Cat Facts!") + cat_gif + encase("p", cat_fact)
+    
+
+# Main Process
+def process(prompt: str) -> str:
+    return preset_2(prompt)
 
 def main():
-    pass
+    print(process("Get me an ice cream!"))
 if __name__ == '__main__':
     main()
